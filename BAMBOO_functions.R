b50087df55d9e1264006b430c16d3678ca3e4587
HiddeMSmits <66623044+HiddeMSmits@users.noreply.github.com>
2024-03-12 15:52:17 +0100

    Create BAMBOO_functions.R

---
+++ BAMBOO_functions.R
@@ -0,0 +1,20 @@
+suppressWarnings(suppressMessages(require(OlinkAnalyze))) #
+# suppressWarnings(suppressMessages(require(RUVSeq))) #
+# suppressWarnings(suppressMessages(require(ggfortify, verbose = F)))
+suppressWarnings(suppressMessages(require(tidyverse))) #
+suppressWarnings(suppressMessages(require(pheatmap))) #
+suppressWarnings(suppressMessages(require(RColorBrewer))) # 
+# suppressWarnings(suppressMessages(require(dbscan))) # 
+suppressWarnings(suppressMessages(require(readr))) #
+library(preprocessCore) #
+suppressWarnings(suppressMessages(require(ggplot2))) #
+
+recode <- dplyr::recode
+select <- dplyr::select
+filter <- dplyr::filter
+mutate <- dplyr::mutate
+summarise <- dplyr::summarise
+group_by <- dplyr::group_by
+case_when <- dplyr::case_when
+
+message("loading the latest function y'all")
