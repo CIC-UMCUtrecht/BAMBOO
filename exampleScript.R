21a9f1c69040f242c8cea3265c0d17583f3843ab
hmsmits <157473457+hmsmits@users.noreply.github.com>
2024-07-04 14:43:04 +0200

    Update exampleScript.R

--- exampleScript.R
+++ exampleScript.R
@@ -7,7 +7,7 @@ source("./GitHub/BAMBOO/BAMBOO_functions.R")
 data_path <- "./GitHub/BAMBOO/data/"
 
 # Load NPX data files (only wide format is supported)
-plate_list <- loadNPXfiles(data_path)
+plate_list <- load_NPX_files(data_path)
 
 # Define reference and subject plates
 reference_plate <- plate_list[[1]]
@@ -21,12 +21,12 @@ normalized_subject_plate <- BAMBOO_normalization(
   reference_plate, 
   subject_plate, 
   BCs = bridging_controls, 
-  LODthreshold = 6
+  LOD_threshold = 6
 )
 
 # Plot bridging controls before and after BAMBOO normalization
-plotBeforeAndAfter(reference_plate, subject_plate, normalized_subject_plate)
+plot_before_and_after(reference_plate, subject_plate, normalized_subject_plate)
 
 # Save the normalized data in wide format
-writeNPX(norm.SubjectPlate, path =  "./GitHub/BAMBOO/normData/", filename = "normalizedSubjectPlate.xlsx")
+write_NPX(normalized_subject_plate, path =  "./GitHub/BAMBOO/normData/", filename = "normalized_subject_plate.xlsx")
 
